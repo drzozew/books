d85cff04c95df8a661ea46c7dbceddc0518a25f2
Dzozew <jex.kara@gmail.com>
2020-06-06 17:00:54 +0200

    add favorite number

--- slowni.py
+++ slowni.py
@@ -6,3 +6,14 @@ persons = {
 }
 
 print(persons)
+
+favorite_numbers = {
+    "badzi": 1,
+    "stefan": 2,
+    "aga": 3,
+    "zin": 4,
+    "eu": 5,
+}
+for name in favorite_numbers:
+    favorite_number = favorite_numbers[name]
+    print(f"Ulubina liczba {name.title()} to {favorite_number}")
