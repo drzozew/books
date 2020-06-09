b00dc4238cafc47ed8595c518c268633c344f3fb
Dzozew <jex.kara@gmail.com>
2020-06-09 22:43:40 +0200

    add files

---
+++ 8.15.py
@@ -0,0 +1,8 @@
+import car_info
+from car_info import cars
+from car_info import cars as cr
+import car_info as ca
+from car_info import *
+
+car = car_info.cars('subaru', 'jakies', color="red")
+print(car)
---
+++ car_info.py
@@ -0,0 +1,4 @@
+def cars(mark, model, **car_info):
+    car_info['marka'] = mark
+    car_info['model'] = model
+    return car_info
