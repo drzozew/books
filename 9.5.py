291ac99f9cdabc4430915bad7b14b8af252a1294
Dzozew <jex.kara@gmail.com>
2020-06-10 14:54:14 +0200

    add files

---
+++ 9.5.py
@@ -0,0 +1,36 @@
+class User():
+    def __init__(self, first_name, Last_name, age, city):
+        self.first_name = first_name
+        self.Last_name = Last_name
+        self.age = age
+        self.city = city
+        self.login_attemps = 0
+
+    def describe_user(self):
+        print(f"{self.first_name.title()} {self.Last_name.title()} ma "
+              f"{self.age} lat i pochodzi z {self.city.title()}")
+
+    def greet_user(self):
+        print(f"Witaj {self.first_name.title()} {self.Last_name.title()}")
+
+    def increment_login_attempts(self):
+        self.login_attemps += 1
+
+    def reset_login_attemps(self):
+        self.login_attemps = 0
+
+
+x = User("badzi", "kan", "22", "kista")
+
+x.increment_login_attempts()
+print(x.login_attemps)
+x.increment_login_attempts()
+print(x.login_attemps)
+x.increment_login_attempts()
+print(x.login_attemps)
+x.increment_login_attempts()
+print(x.login_attemps)
+x.reset_login_attemps()
+print(x.login_attemps)
+x.increment_login_attempts()
+print(x.login_attemps)
---
+++ 9.6.py
@@ -0,0 +1,42 @@
+class Res():
+    'tworzy obiekt restaurant'
+
+    def __init__(self, restaurant_name, cuisine_type):
+        '''inicjalizacja atrybutów name i cuisine'''
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_serverd = 0
+
+    def describe_restaurant(self):
+        print(f"{self.restaurant_name}i {self.cuisine_type}")
+
+    def open_restaurant(self):
+        print("Restauracja jest otwarta o 18:00")
+
+    def read_served(self):
+        print(f"Ilość obsłużonych {self.number_serverd}")
+
+    def served_update(self, person):
+        self.number_serverd += person
+
+    def set_served(self, update_persona):
+        self.number_serverd = update_persona
+
+
+class IceCreamStand(Res):
+
+    def __init__(self, restaurant_name, cuisine_type):
+        super().__init__(restaurant_name, cuisine_type)
+        self.list_of_ices = []
+
+    def ice_cream(self, list_of_ices):
+        print("Ice list: ")
+        for ice in list_of_ices:
+            print(f"- {ice}")
+
+
+ices = ["włoskie", "calipso", "ciepłe"]
+
+x = IceCreamStand("Lodek", "Włoskie")
+x.describe_restaurant()
+x.ice_cream(ices)
